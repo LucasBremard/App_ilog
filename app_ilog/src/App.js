2c1a6155fafcbf0bd014129b55086d6ea576d9aa
Lucas Bremard <72856710+LucasBremard@users.noreply.github.com>
2023-07-18 16:27:42 +0200

    implementation of base files

--- app_ilog/src/App.js
+++ app_ilog/src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import StockManager from './components/StockManager';
+import SubjectPage from './components/SubjectPage';
+import ThirdPage from './components/ThirdPage';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('first');
+  const [stockList, setStockList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchVisible, setSearchVisible] = useState(false);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleAddStock = (newStockItem) => {
+    setStockList([...stockList, newStockItem]);
+  };
+
+  const handleDeleteStock = (index) => {
+    const updatedStockList = [...stockList];
+    updatedStockList.splice(index, 1);
+    setStockList(updatedStockList);
+  };
+
+  const handleSearchInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearchButtonClick = () => {
+    setSearchVisible((prevVisible) => !prevVisible);
+    setSearchTerm('');
+  };
+
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentPage === 'first' ? (
+        <div>
+          <header className="header">
+            <h1>Storage Management</h1>
+          </header>
+          <nav>
+            <div className="button-container">
+              <button onClick={() => handlePageChange('second')}>Subject</button>
+              <button onClick={() => handlePageChange('third')}>Grid</button>
+
+            </div>
+          </nav>
+          {searchVisible && (
+            <div className="search-bar">
+              <input
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleSearchInputChange}
+              />
+            </div>
+          )}
+          <StockManager
+            stockList={stockList}
+            onAddStock={handleAddStock}
+            onDeleteStock={handleDeleteStock}
+          />
+        </div>
+      ) : currentPage === 'second' ? (
+        <SubjectPage
+          stockList={stockList}
+          handlePageChange={handlePageChange}
+        />
+      ) : (
+        <ThirdPage
+          handlePageChange={handlePageChange}
+        />
+      )}
+      <footer>
+        Lucas BREMARD - IMT Nord Europe - {currentDateTime.toLocaleString()}
+      </footer>
     </div>
   );
 }
