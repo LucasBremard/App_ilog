1c7999eed3d553b3b4c04fa76eb4e30b4160869d
Lucas Bremard <72856710+LucasBremard@users.noreply.github.com>
2023-07-19 11:12:25 +0200

    removal of non-functional search bar

--- app_ilog/src/App.js
+++ app_ilog/src/App.js
@@ -4,41 +4,37 @@ import SubjectPage from './components/SubjectPage';
 import ThirdPage from './components/ThirdPage';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('first');
-  const [stockList, setStockList] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchVisible, setSearchVisible] = useState(false);
+  // State variables
+  const [currentPage, setCurrentPage] = useState('first'); // State to track the current page being displayed
+  const [stockList, setStockList] = useState([]); // State to manage the list of stocks
 
+  // Function to handle page changes when clicking buttons in the navigation
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
+  // Function to add a new stock item to the list
   const handleAddStock = (newStockItem) => {
     setStockList([...stockList, newStockItem]);
   };
 
+  // Function to delete a stock item from the list
   const handleDeleteStock = (index) => {
     const updatedStockList = [...stockList];
     updatedStockList.splice(index, 1);
     setStockList(updatedStockList);
   };
 
-  const handleSearchInputChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
-  const handleSearchButtonClick = () => {
-    setSearchVisible((prevVisible) => !prevVisible);
-    setSearchTerm('');
-  };
-
+  // State variable to store the current date and time
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
 
+  // Effect to update the current date and time every second using setInterval
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentDateTime(new Date());
     }, 1000);
 
+    // Cleanup function to clear the interval when the component is unmounted
     return () => {
       clearInterval(interval);
     };
@@ -47,27 +43,21 @@ function App() {
   return (
     <div className="App">
       {currentPage === 'first' ? (
+        // Display StockManager component if the current page is 'first'
         <div>
           <header className="header">
             <h1>Storage Management</h1>
           </header>
           <nav>
             <div className="button-container">
+              {/* Button to switch to the 'Subject' page */}
               <button onClick={() => handlePageChange('second')}>Subject</button>
+              {/* Button to switch to the 'Grid' page */}
               <button onClick={() => handlePageChange('third')}>Grid</button>
-
             </div>
           </nav>
-          {searchVisible && (
-            <div className="search-bar">
-              <input
-                type="text"
-                placeholder="Search..."
-                value={searchTerm}
-                onChange={handleSearchInputChange}
-              />
-            </div>
-          )}
+          {/* Display the search bar if searchVisible is true */}
+          {/* Render the StockManager component with required props */}
           <StockManager
             stockList={stockList}
             onAddStock={handleAddStock}
@@ -75,15 +65,18 @@ function App() {
           />
         </div>
       ) : currentPage === 'second' ? (
+        // Display SubjectPage component if the current page is 'second'
         <SubjectPage
           stockList={stockList}
           handlePageChange={handlePageChange}
         />
       ) : (
+        // Display ThirdPage component if the current page is 'third'
         <ThirdPage
           handlePageChange={handlePageChange}
         />
       )}
+      {/* Footer displaying author information and the current date and time */}
       <footer>
         Lucas BREMARD - IMT Nord Europe - {currentDateTime.toLocaleString()}
       </footer>
